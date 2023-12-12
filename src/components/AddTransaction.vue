4a97704db16176637c95d1d5b5d1c7fc762f14fe
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-12-12 15:54:18 +0600

    start of form

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,21 +1,36 @@
 <template>
   <h3>Add new transaction</h3>
-  <form id="form">
+  <form id="form" @submit.prevent="onSubmit">
     <div class="form-control">
       <label for="text">Text</label>
-      <input type="text" id="text" placeholder="Enter text..." />
+      <input type="text" id="text" placeholder="Enter text..." v-model="text" />
     </div>
     <div class="form-control">
       <label for="amount"
         >Amount <br />
         (negative - expense, positive - income)</label
       >
-      <input type="number" id="amount" placeholder="Enter amount..." />
+      <input
+        type="number"
+        id="amount"
+        placeholder="Enter amount..."
+        v-model="amount"
+      />
     </div>
     <button class="btn">Add transaction</button>
   </form>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
 
-<style lang="scss" scoped></style>
+const text = ref("");
+const amount = ref(0);
+
+const onSubmit = () => {
+  
+  console.log(text.value, amount.value);
+};
+</script>
+
+<style lang="css" scoped></style>
